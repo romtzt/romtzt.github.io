e5c8ad39ebecceb3af525f8c57919bc8ffa18a4d
romtzt <145071112+romtzt@users.noreply.github.com>
2025-04-07 12:43:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
-window.mdp = "1234";
+window.mdp = "2212";
 
-window.srciframe = "https://docs.google.com/spreadsheets/d/1qGmJf-NdUpyF6g8orhqucjMM2VSbic-oOBrHEtfTWqE/edit?widget=true&headers=false#gid=1566734167"
+window.srciframe = "https://docs.google.com/spreadsheets/d/1qGmJf-NdUpyF6g8orhqucjMM2VSbic-oOBrHEtfTWqE/edit?widget=true&headers=false#gid=1402039823"
